5737da5ed374afb4040e900bf09da6db014a966e
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-23 14:19:34 -0300

    rename na idPet.

--- src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDTO.java
+++ src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDTO.java
@@ -1,4 +1,4 @@
 package br.com.alura.adopet.api.dto;
 
-public record SolicitacaoAdocaoDTO(Long id, Long idTutor, String Motivo) {
+public record SolicitacaoAdocaoDTO(Long idPet, Long idTutor, String Motivo) {
 }
