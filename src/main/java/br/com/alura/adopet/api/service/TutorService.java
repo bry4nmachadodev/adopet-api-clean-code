37bea51afb9229b7a71a46951eb59c7939296f3a
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 14:15:31 -0300

    criação da TutorService.

---
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -0,0 +1,21 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TutorService {
+
+    @Autowired
+    private TutorRepository repository;
+
+    public void cadastrar(){
+
+    }
+
+    public void atualizar(){
+
+    }
+}
