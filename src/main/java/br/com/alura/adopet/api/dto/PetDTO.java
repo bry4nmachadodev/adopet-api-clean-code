fe70dfad50c6d031779579e87ae03a6df5914453
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 13:58:23 -0300

    mudança na dto pet.

--- src/main/java/br/com/alura/adopet/api/dto/PetDTO.java
+++ src/main/java/br/com/alura/adopet/api/dto/PetDTO.java
@@ -1,6 +1,7 @@
 package br.com.alura.adopet.api.dto;
 
+import br.com.alura.adopet.api.model.TipoPet;
 import jakarta.validation.constraints.NotBlank;
 
-public record PetDTO(@NotBlank String nome, @NotBlank String tipo, @NotBlank String raca) {}
+public record PetDTO(@NotBlank String nome, @NotBlank TipoPet tipo, @NotBlank String raca) {}
 
