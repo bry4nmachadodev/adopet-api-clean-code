cd523c670bf1294a461ac4fae5c791a522a675b5
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 14:32:00 -0300

    criaçãos dos métodos com o recebimento de DTO nos parametros.

--- src/main/java/br/com/alura/adopet/api/service/TutorService.java
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -1,8 +1,11 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.dto.TutorAtualizacaoDTO;
+import br.com.alura.adopet.api.dto.TutorCadastroDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +14,24 @@ public class TutorService {
     @Autowired
     private TutorRepository repository;
 
-    public void cadastrar(){
+    public void cadastrar(TutorCadastroDTO dto){
+        boolean telefoneJaCadastrado = repository.existsByTelefone(dto.telefone());
+        boolean emailJaCadastrado = repository.existsByEmail(dto.email());
 
+        if (telefoneJaCadastrado || emailJaCadastrado) {
+            throw new ValidacaoException("Dados já cadastrados para outro tutor!");
+        }
+
+        Tutor tutor = new Tutor(dto.nome(), dto.email(), dto.telefone());
+        repository.save(tutor);
     }
 
-    public void atualizar(){
-
+    public void atualizar(TutorAtualizacaoDTO dto) {
+        Tutor tutor = repository.getReferenceById(dto.id());
+        tutor.setNome(dto.nome());
+        tutor.setEmail(dto.email());
+        tutor.setTelefone(dto.telefone());
+        repository.save(tutor);
     }
+
 }
