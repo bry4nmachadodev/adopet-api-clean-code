78585127d2680e1c57c1392b0d506b60ebbed326
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 13:40:14 -0300

    criação da interface para juntas todas as validações

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/abrigo/ValidadorCadastroAbrigo.java
@@ -0,0 +1,7 @@
+package br.com.alura.adopet.api.validacoes.abrigo;
+
+import br.com.alura.adopet.api.dto.AbrigoDTO;
+
+public interface ValidadorCadastroAbrigo {
+    void validar(AbrigoDTO dto);
+}
