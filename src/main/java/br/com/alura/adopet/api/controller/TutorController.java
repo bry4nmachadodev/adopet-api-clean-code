ac587618f64837b36ad0d4aef25b7f7398d945dc
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 14:40:18 -0300

    Recebendo DTO no controller e tratando suas excecões caso caia nelas.

--- src/main/java/br/com/alura/adopet/api/controller/TutorController.java
+++ src/main/java/br/com/alura/adopet/api/controller/TutorController.java
@@ -1,8 +1,12 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.TutorAtualizacaoDTO;
+import br.com.alura.adopet.api.dto.TutorCadastroDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Tutor;
-import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.service.TutorService;
 import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,27 +17,28 @@ import org.springframework.web.bind.annotation.*;
 public class TutorController {
 
     @Autowired
-    private TutorRepository repository;
+    private TutorService tutorService;
 
     @PostMapping
     @Transactional
-    public ResponseEntity<String> cadastrar(@RequestBody @Valid Tutor tutor) {
-        boolean telefoneJaCadastrado = repository.existsByTelefone(tutor.getTelefone());
-        boolean emailJaCadastrado = repository.existsByEmail(tutor.getEmail());
-
-        if (telefoneJaCadastrado || emailJaCadastrado) {
-            return ResponseEntity.badRequest().body("Dados já cadastrados para outro tutor!");
-        } else {
-            repository.save(tutor);
-            return ResponseEntity.ok().build();
+    public ResponseEntity<String> cadastrar(@RequestBody @Valid TutorCadastroDTO dto) {
+        try {
+            tutorService.cadastrar(dto);
+            return ResponseEntity.ok("Tutor cadastrado com sucesso!");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @PutMapping
     @Transactional
-    public ResponseEntity<String> atualizar(@RequestBody @Valid Tutor tutor) {
-        repository.save(tutor);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> atualizar(@RequestBody @Valid TutorAtualizacaoDTO dto) {
+        try{
+            tutorService.atualizar(dto);
+            return ResponseEntity.ok("Tutor atualizado com sucesso!");
+        } catch (ValidationException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
