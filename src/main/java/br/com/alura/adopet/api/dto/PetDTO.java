fa27713365d69de16b504f094cafdc16da5678c0
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 14:04:54 -0300

    fix nas validações da PetDTO.

--- src/main/java/br/com/alura/adopet/api/dto/PetDTO.java
+++ src/main/java/br/com/alura/adopet/api/dto/PetDTO.java
@@ -2,6 +2,7 @@ package br.com.alura.adopet.api.dto;
 
 import br.com.alura.adopet.api.model.TipoPet;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record PetDTO(@NotBlank String nome, @NotBlank TipoPet tipo, @NotBlank String raca) {}
+public record PetDTO(@NotBlank String nome, @NotNull TipoPet tipo, @NotBlank String raca) {}
 
