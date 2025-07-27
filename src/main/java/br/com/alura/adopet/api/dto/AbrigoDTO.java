8aa2bd3ba364817c4f2416b4c0ad20cda86ed1b6
Bryan Machado <bryanmachado1703@icloud.com>
2025-07-27 13:55:37 -0300

    mudança na dtoabrigo

--- src/main/java/br/com/alura/adopet/api/dto/AbrigoDTO.java
+++ src/main/java/br/com/alura/adopet/api/dto/AbrigoDTO.java
@@ -2,4 +2,4 @@ package br.com.alura.adopet.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record AbrigoDTO(@NotBlank String nome, @NotBlank String numero, @NotBlank String email) {}
+public record AbrigoDTO(@NotBlank String nome, @NotBlank String telefone, @NotBlank String email) {}
